1618e696fc11e22e88b203f11ea9f46b8bca4894
Mark Ormesher <me@markormesher.co.uk>
2016-11-16 13:57:14 +0000

    Printing tests

--- src/main/kotlin/uk/co/markormesher/easymaps/engine/helpers/Printing.kt
+++ src/main/kotlin/uk/co/markormesher/easymaps/engine/helpers/Printing.kt
@@ -1,23 +1,27 @@
 package uk.co.markormesher.easymaps.engine.helpers
 
-private val ANSI_RESET = "\u001B[0m"
-private val ANSI_BOLD = "\u001B[1m"
-private val ANSI_WHITE_BG = "\u001B[107m"
-private val ANSI_BLACK_TEXT = "\u001B[30m"
-private val ANSI_RED_TEXT = "\u001B[31m"
-private val ANSI_GREEN_TEXT = "\u001B[32m"
-private val ANSI_YELLOW_TEXT = "\u001B[33m"
-private val ANSI_BLUE_TEXT = "\u001B[34m"
-private val ANSI_PURPLE_TEXT = "\u001B[35m"
-private val ANSI_CYAN_TEXT = "\u001B[36m"
-private val ANSI_WHITE_TEXT = "\u001B[37m"
+internal val ANSI_RESET = "\u001B[0m"
+internal val ANSI_BOLD = "\u001B[1m"
+internal val ANSI_WHITE_BG = "\u001B[107m"
+internal val ANSI_BLACK_TEXT = "\u001B[30m"
+internal val ANSI_RED_TEXT = "\u001B[31m"
+internal val ANSI_GREEN_TEXT = "\u001B[32m"
+internal val ANSI_YELLOW_TEXT = "\u001B[33m"
+internal val ANSI_BLUE_TEXT = "\u001B[34m"
+internal val ANSI_PURPLE_TEXT = "\u001B[35m"
+internal val ANSI_CYAN_TEXT = "\u001B[36m"
+internal val ANSI_WHITE_TEXT = "\u001B[37m"
 
-private fun bold(msg: String): String = ANSI_BOLD + msg + ANSI_RESET
-private fun whiteBg(msg: String): String = ANSI_WHITE_BG + msg + ANSI_RESET
-private fun blackText(msg: String): String = ANSI_BLACK_TEXT + msg + ANSI_RESET
-private fun redText(msg: String): String = ANSI_RED_TEXT + msg + ANSI_RESET
-private fun yellowText(msg: String): String = ANSI_YELLOW_TEXT + msg + ANSI_RESET
-private fun purpleText(msg: String): String = ANSI_PURPLE_TEXT + msg + ANSI_RESET
+internal fun bold(msg: String): String = ANSI_BOLD + msg + ANSI_RESET
+internal fun whiteBg(msg: String): String = ANSI_WHITE_BG + msg + ANSI_RESET
+internal fun blackText(msg: String): String = ANSI_BLACK_TEXT + msg + ANSI_RESET
+internal fun redText(msg: String): String = ANSI_RED_TEXT + msg + ANSI_RESET
+internal fun greenText(msg: String): String = ANSI_GREEN_TEXT + msg + ANSI_RESET
+internal fun yellowText(msg: String): String = ANSI_YELLOW_TEXT + msg + ANSI_RESET
+internal fun blueText(msg: String): String = ANSI_BLUE_TEXT + msg + ANSI_RESET
+internal fun purpleText(msg: String): String = ANSI_PURPLE_TEXT + msg + ANSI_RESET
+internal fun cyanText(msg: String): String = ANSI_CYAN_TEXT + msg + ANSI_RESET
+internal fun whiteText(msg: String): String = ANSI_WHITE_TEXT + msg + ANSI_RESET
 
 val INPUT_PROMPT = ANSI_CYAN_TEXT + ANSI_BOLD + " --> " + ANSI_RESET
 
---
+++ src/test/kotlin/uk/co/markormesher/easymaps/engine/helpers/PrintingTest.kt
@@ -0,0 +1,129 @@
+package uk.co.markormesher.easymaps.engine.helpers
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import kotlin.test.assertEquals
+
+class PrintingTest {
+
+	var originalStdOut: PrintStream? = null
+	val stdOut = ByteArrayOutputStream()
+
+	@Before
+	fun setupStreams() {
+		originalStdOut = System.out
+		System.setOut(PrintStream(stdOut))
+	}
+
+	@After
+	fun cleanUpStreams() {
+		System.setOut(originalStdOut)
+	}
+
+	@Test
+	fun formattingConstantsShouldBeCorrect() {
+		assertEquals("\u001B[0m", ANSI_RESET)
+		assertEquals("\u001B[1m", ANSI_BOLD)
+		assertEquals("\u001B[107m", ANSI_WHITE_BG)
+		assertEquals("\u001B[30m", ANSI_BLACK_TEXT)
+		assertEquals("\u001B[31m", ANSI_RED_TEXT)
+		assertEquals("\u001B[32m", ANSI_GREEN_TEXT)
+		assertEquals("\u001B[33m", ANSI_YELLOW_TEXT)
+		assertEquals("\u001B[34m", ANSI_BLUE_TEXT)
+		assertEquals("\u001B[35m", ANSI_PURPLE_TEXT)
+		assertEquals("\u001B[36m", ANSI_CYAN_TEXT)
+		assertEquals("\u001B[37m", ANSI_WHITE_TEXT)
+	}
+
+	@Test
+	fun formattingShortcutsCouldBeCorrect() {
+		val str = "test"
+		assertEquals("\u001B[1m$str\u001B[0m", bold(str))
+		assertEquals("\u001B[107m$str\u001B[0m", whiteBg(str))
+		assertEquals("\u001B[30m$str\u001B[0m", blackText(str))
+		assertEquals("\u001B[31m$str\u001B[0m", redText(str))
+		assertEquals("\u001B[32m$str\u001B[0m", greenText(str))
+		assertEquals("\u001B[33m$str\u001B[0m", yellowText(str))
+		assertEquals("\u001B[34m$str\u001B[0m", blueText(str))
+		assertEquals("\u001B[35m$str\u001B[0m", purpleText(str))
+		assertEquals("\u001B[36m$str\u001B[0m", cyanText(str))
+		assertEquals("\u001B[37m$str\u001B[0m", whiteText(str))
+	}
+
+	@Test
+	fun inputPromptConstantShouldBeCorrect() {
+		assertEquals("\u001B[36m\u001B[1m --> \u001B[0m", INPUT_PROMPT)
+	}
+
+	@Test
+	fun printHeaderShouldFormatCorrectly() {
+		val expected = "\n" +
+				"\u001B[107m\u001B[31m ~~~~~~~~ \u001B[0m\u001B[0m\n" +
+				"\u001B[107m\u001B[31m ~ \u001B[0m\u001B[0m" +
+				"\u001B[1m\u001B[107m\u001B[31mTEST\u001B[0m\u001B[0m\u001B[0m" +
+				"\u001B[107m\u001B[31m ~ \u001B[0m\u001B[0m\n" +
+				"\u001B[107m\u001B[31m ~~~~~~~~ \u001B[0m\u001B[0m\n" +
+				"\n"
+		printHeader("TEST")
+		assertEquals(expected, stdOut.toString())
+	}
+
+	@Test
+	fun printSubHeaderShouldFormatCorrectly() {
+		val expected = "\n" +
+				"\u001B[107m\u001B[35m ------ \u001B[0m\u001B[0m\n" +
+				"\u001B[107m  \u001B[0m" +
+				"\u001B[1m\u001B[107m\u001B[30mTEST\u001B[0m\u001B[0m\u001B[0m" +
+				"\u001B[107m  \u001B[0m\n" +
+				"\u001B[107m\u001B[35m ------ \u001B[0m\u001B[0m\n" +
+				"\n"
+		printSubHeader("TEST")
+		assertEquals(expected, stdOut.toString())
+	}
+
+	@Test
+	fun printInfoShouldFormatCorrectly() {
+		val expected = "\r \u001B[1m\u001B[33m INFO:\u001B[0m\u001B[0m TEST\n"
+		printInfo("TEST")
+		assertEquals(expected, stdOut.toString())
+	}
+
+	@Test
+	fun printWarningShouldFormatCorrectly() {
+		val expected = "\r \u001B[1m\u001B[35m WARN:\u001B[0m\u001B[0m TEST\n"
+		printWarning("TEST")
+		assertEquals(expected, stdOut.toString())
+	}
+
+	@Test
+	fun printErrorShouldFormatCorrectly() {
+		val expected = "\r \u001B[1m\u001B[31mERROR:\u001B[0m\u001B[0m TEST\n"
+		printError("TEST")
+		assertEquals(expected, stdOut.toString())
+	}
+
+	@Test
+	fun printSubInfoShouldFormatCorrectly() {
+		val expected = "\r        -  TEST\n"
+		printSubInfo("TEST")
+		assertEquals(expected, stdOut.toString())
+	}
+
+	@Test
+	fun printSubWarningShouldFormatCorrectly() {
+		val expected = "\r        - \u001B[1m\u001B[35m WARN:\u001B[0m\u001B[0m TEST\n"
+		printSubWarning("TEST")
+		assertEquals(expected, stdOut.toString())
+	}
+
+	@Test
+	fun printSubErrorShouldFormatCorrectly() {
+		val expected = "\r        - \u001B[1m\u001B[31mERROR:\u001B[0m\u001B[0m TEST\n"
+		printSubError("TEST")
+		assertEquals(expected, stdOut.toString())
+	}
+
+}
